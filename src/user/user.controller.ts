d6fa46755efef893b32b9aed279f29b9e8dd5655
chakyubeom <ckrbqja@gmail.com>
2024-06-23 23:10:02 +0900

    임시

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,8 @@ import { CurrentUser } from "../auth/jwt/getUser.decorator";
 import { UserLoginType } from "../auth/userLogin.type";
 import { ApiTags } from "@nestjs/swagger";
 
+type LoginRes = { id: number; name: string };
+
 @Controller('users')
 @ApiTags('user')
 export class UserController {
@@ -13,7 +15,7 @@ export class UserController {
   @TypedRoute.Get()
   async currentUserInfo(
     @CurrentUser() { id }: UserLoginType,
-  ): Promise<{ id: number; name: string }> {
+  ): Promise<LoginRes> {
     const userEntities = await this.userService.currentUserInfo(id);
     return { id: userEntities!.id, name: userEntities!.name };
   }
