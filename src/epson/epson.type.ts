f4b221dc11bf85b93c9d7bede57e4b9b911d6ab7
chakyubeom <ckrbqja@gmail.com>
2024-06-23 23:08:38 +0900

    임시

--- src/epson/epson.type.ts
+++ src/epson/epson.type.ts
@@ -19,23 +19,23 @@ export type PrintJobDataType = {
 };
 export type PrintSettingType = {
   //용지 크기 (ms_a4 - A4용지)
-  media_size: 'ms_a4' | 'ms_a3';
+  media_size?: 'ms_a4' | 'ms_a3';
   //용지 종류 (mt_plainpaper - )
-  media_type: 'mt_plainpaper';
+  media_type?: 'mt_plainpaper';
   //인쇄시 테투리 인쇄 설정 (false - 테투리있는 인쇄)
-  borderless: boolean;
+  borderless?: boolean;
   //인쇄 품질 (normal - 보통)
-  print_quality: 'normal';
+  print_quality?: 'normal';
   //급지 방법 (front2 - 전면 2번째 용지함)
-  source: 'front1' | 'front2';
+  source?: 'front1' | 'front2';
   //색상모드 (mono - 흑백)
-  color_mode: 'mono' | 'color';
+  color_mode?: 'mono' | 'color';
   //양면인쇄 (none - 단면인쇄)
-  '2_sided': 'none';
+  '2_sided'?: 'none';
   //역순인쇄 (false - 첫페이지부터 인쇄)
-  reverse_order: boolean;
+  reverse_order?: boolean;
   //사본인쇄 (1 - 기본 설정, 1부만 인쇄)
-  copies: number;
+  copies?: number;
   //묶음 인쇄 방법 (true - 세트로 인쇄, false - 페이지별 인쇄)
-  collate: boolean;
+  collate?: boolean;
 };
